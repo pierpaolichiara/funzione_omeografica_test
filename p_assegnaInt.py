a7df60ada0bd5d921d8ee347e8915dcbd2a7c275
pierpaolichiara <pierpaolichiara@gmail.com>
2023-04-13 16:37:13 +0200

    aggiunto file di input CLASSE_1A.xlsx
    aggiunto file CLASSE_1A.xlsx

---
+++ p_assegnaInt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 13:07:50 2023
+
+@author: Matteo
+"""
+
+import array as arr
+import numpy as np
+import fz_genera_dominio_coeff
+
+def assegna_abcd(domini_abcd): 
+    print(domini_abcd)
+    return
+
+assegna_abcd
---
+++ progetto.py
@@ -0,0 +1,35 @@
+""" 
+Progetto per docenti di matematica delle scuole secondarie superiori:
+Questo programma e' pensato per produrre test a risposta aperta sulla funzione 
+omeografica f(x)=(ax +b)/(cx+d) da somministrare a una classe di studenti.
+Ogni coefficiente della funzione viene scelto in maniera casuale all'interno 
+di un dominio da specificare. A ogni alunno il programma associa una quaterna 
+di coefficienti [a,b,c,d] diversa, e per ogni alunno stampa il testo del test #in formato...##. 
+L'associazione tra cognome dell'alunno e quaterna di coefficienti viene 
+stampata su un file excel disponibile per l'insegnante'
+
+    
+
+
+"""
+import numpy as np
+from numpy import random
+from random import random
+import sympy as sp
+from sympy import symbols
+import funzione_omeografica
+
+
+
+
+import array as arr
+
+a,b,c,d=1,3,5,7
+parametri=arr.array('i',[a,b,c,d])
+abcd=[a,b,c,d]
+print (parametri)
+print(abcd)
+y=funzione_omeografica.genera_fz_omeografica(a,b,c,d,)
+print ("f(x)=",y)
+
+
