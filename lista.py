c19d05d4f1386e816fc7ab8f368d7806a4cae0d8
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-05 16:24:32 +0200

    aggiunto assegna_abcd.py
    file da sostituire a fz_genera_coeff_abcd e usare insieme a dominio_int

--- lista.py
+++ lista.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr  7 16:16:26 2023
 
-@author: Matteo
 """
 
 import pandas as pd
-import fz_genera_coeff_abcd0
+import fz_genera_coeff_abcd
 
 dataframe=pd.read_excel("CLASSE_1A.xlsx", index_col=0)
 print(dataframe)
@@ -15,7 +13,7 @@ print(dataframe)
 zeta={}
 for name in dataframe["COGNOME"]:
     #print(name)
-    zeta[name]=fz_genera_coeff_abcd0.assegna_abcd()
+    zeta[name]=fz_genera_coeff_abcd.assegna_abcd()
 print(zeta)
 
 dati=pd.DataFrame.from_dict(zeta).transpose()
