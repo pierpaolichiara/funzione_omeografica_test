d9bfdd749dfb8e27f5ce29204fdcf01d8ce1caaf
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-18 15:39:19 +0200

    Update test_hyp_parse_function_domain.py
    aggiunto test_lenght che controlla che la lunghezza della tupla corrispondente agli estremi del dominio sia 2

--- funzione_omeografica_test/test/test_hyp_parse_function_domain.py
+++ funzione_omeografica_test/test/test_hyp_parse_function_domain.py
@@ -21,3 +21,14 @@ def test_float(domain_extremes):
     domain_extremes_str = str(domain_extremes)
     with pytest.raises(ValueError):
         extrs = parse_function_domain(domain_extremes_str)
+
+@given(domain_extremes=st.lists(st.integers(), min_size=2, max_size=2))
+def test_lenght(domain_extremes):
+    """
+    questo test verifica che la lunghezza della tupla generata da parse_function_domain, corrispondente
+    agli estremi del dominio sia 2
+    """
+    print(domain_extremes)
+    domain_extremes_str = str(domain_extremes)
+    extrs = parse_function_domain(domain_extremes_str)
+    assert len(extrs)==2
