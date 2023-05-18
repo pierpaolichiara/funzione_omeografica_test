3546dcd1d31284ea64c00e4a58d4bc476ec7897d
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-19 00:00:03 +0200

    updated test_parse_function_domain.py
    uniti i test per la funzione  in un unico file, aggiunte descrizioni e rinominati i test:
    test_float->test_floats_parse_function_domain
    test_parse_function_domain_raises->test_raises_parse_function_domain
    test_lenght->test_lenght_parse_function_domain

--- funzione_omeografica_test/test/test_hyp_parse_function_domain.py
+++ funzione_omeografica_test/test/test_hyp_parse_function_domain.py
@@ -2,33 +2,28 @@ import pytest
 import hypothesis
 from hypothesis import given
 import hypothesis.strategies as st
-
-#import funzione_omeografica_test.generate_abcd_omeo as lib
-
-def parse_function_domain(domain_extremes: str) -> tuple:
-    extrs = [int(piece.strip("()[], ")) for piece in domain_extremes.split(",")]
-    return tuple(extrs)
-"""
-@given(domain_extremes=st.floats())
-def test_float(domain_extremes):
-    extrs=parse_function_domain(domain_extremes)
-    assert extrs == ValueError
-"""
+import funzione_omeografica_test.generate_abcd_omeo as lib
 
 @given(domain_extremes=st.lists(st.floats()))
-def test_float(domain_extremes):
+def test_float_parse_function_domain(domain_extremes):
+    """Data una lista di decimali il test verifica che la funzione parse_function_domain riscontri un ValueError"""
     print(domain_extremes)
     domain_extremes_str = str(domain_extremes)
     with pytest.raises(ValueError):
-        extrs = parse_function_domain(domain_extremes_str)
+        extrs = lib.parse_function_domain(domain_extremes_str)
 
-@given(domain_extremes=st.lists(st.integers(), min_size=2, max_size=2))
-def test_lenght(domain_extremes):
+@given(domain_extremes=st.lists(st.integers(), #utilizziamo una lista di interi perche' e' l'input che ci aspettiamo
+                                               # e che ci interessa verificare, e per escludere dal test casi con
+                                               # eventuali errori di conversione da lista a tupla dovuti al separatore dei decimali
+                                min_size=2,
+                                max_size=2)
+       )
+def test_lenght_parse_function_domain(domain_extremes):
     """
-    questo test verifica che la lunghezza della tupla generata da parse_function_domain, corrispondente
-    agli estremi del dominio sia 2
+    Data una lista di due interi, il test verifica che la tupla associata alla lista generata da parse_function_domain
+    abbia lunghezza 2.
     """
     print(domain_extremes)
     domain_extremes_str = str(domain_extremes)
-    extrs = parse_function_domain(domain_extremes_str)
+    extrs = lib.parse_function_domain(domain_extremes_str)
     assert len(extrs)==2
--- funzione_omeografica_test/test/test_parse_function_domain.py
+++ funzione_omeografica_test/test/test_parse_function_domain.py
@@ -1,4 +1,7 @@
 import pytest
+import hypothesis
+from hypothesis import given
+import hypothesis.strategies as st
 import funzione_omeografica_test.generate_abcd_omeo as lib
 
 test_list = [
@@ -23,11 +26,36 @@ test_list_no_int = [
     ]
 
 @pytest.mark.parametrize(('input_str, expected_result'), test_list_no_int)
-def test_parse_function_domain_raises(input_str, expected_result):
+def test_raises_parse_function_domain(input_str, expected_result):
     with pytest.raises(expected_result):
         input_values = input_str
         extrs = lib.parse_function_domain(input_values)
         assert extrs == expected_result
+
+
+@given(domain_extremes=st.lists(st.floats()))
+def test_floats_parse_function_domain(domain_extremes):
+    """Data una lista di decimali il test verifica che la funzione parse_function_domain riscontri un ValueError"""
+    print(domain_extremes)
+    domain_extremes_str = str(domain_extremes)
+    with pytest.raises(ValueError):
+        extrs = lib.parse_function_domain(domain_extremes_str)
+
+@given(domain_extremes=st.lists(st.integers(), #utilizziamo una lista di interi perche' e' l'input che ci aspettiamo
+                                               # e che ci interessa verificare, e per escludere dal test casi con
+                                               # eventuali errori di conversione da lista a tupla dovuti al separatore dei decimali
+                                min_size=2,
+                                max_size=2)
+       )
+def test_lenght_parse_function_domain(domain_extremes):
+    """
+    Data una lista di due interi, il test verifica che la tupla associata alla lista generata da parse_function_domain
+    abbia lunghezza 2.
+    """
+    print(domain_extremes)
+    domain_extremes_str = str(domain_extremes)
+    extrs = lib.parse_function_domain(domain_extremes_str)
+    assert len(extrs)==2
 
 #TODO: idee di test
 
