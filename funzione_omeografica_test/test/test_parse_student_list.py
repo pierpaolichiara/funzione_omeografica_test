464bd85c8afbcb3fef40661515ed4afb881955e2
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-21 22:29:33 +0200

    Update test_parse_student_list.py
    Aggiunto test_not_xls_parse_student_list

--- funzione_omeografica_test/test/test_parse_student_list.py
+++ funzione_omeografica_test/test/test_parse_student_list.py
@@ -5,10 +5,24 @@ import pytest
 from hypothesis import given
 import hypothesis.strategies as st
 import funzione_omeografica_test.parse_student_list as mod
+import pathlib
 
 @given(path=st.text())
 def test_not_path_parse_student_list(path:str):
     expected_error = IOError
     with pytest.raises(expected_error):
         assert mod.parse_student_list(path)
+
+
+def test_not_xls_parse_student_list():
+    """
+    Dato l'indirizzo assoluto del file corrente, che non e' un *.xls ne' un  *.xlsx,
+    il test verifica che la funzione parse_student_list fornisca il corretto tipo di errore
+    """
+    #ci serve un indirizzo assoluto di file esistente, e valido per test effettuati su ogni dispositivo:
+    #utilizziamo l'indirizzo assoluto del file corrente, che e' in formato .py, nel dispositivo in uso
+    es=os.path.abspath(__file__)
+    expected_error = TypeError
+    with pytest.raises(expected_error):
+        assert mod.parse_student_list(es)
 
