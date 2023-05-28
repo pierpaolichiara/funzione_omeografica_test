53751867d26b9eb68aa9e7d48560a767156a0b32
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-28 16:44:04 +0200

    renamed utils.py -> get_output_folder.py

--- funzione_omeografica_test/get_output_folder.py
+++ funzione_omeografica_test/get_output_folder.py
@@ -1,9 +1,21 @@
 import os
 
 def get_output_folder(output_folder: str = None) -> str:  # indirizzo assoluto
+    """"Questa funzione crea una cartella (cartella_output) vuota allo stesso livello del file corrente se l'input non esiste,
+        altrimenti la crea a partire dall'input indicato.
+
+        Input
+        -----
+        output_folder: str
+            eventuale cartella di cui si verifica l'esistenza, inizializzata a None
+
+        Output
+        ------
+        str:  cartella_output
+        """
     if output_folder:
         if os.path.isabs(output_folder):
-            # se il path è assoluto (es. C:/Users/Matteo/...) lo prendiamo così com'è
+            # se il path è assoluto lo prendiamo così com'è
             cartella_output = output_folder
         else:
             # altrimenti è un path relativo, e creiamo una sottocartella nella cartella dove l'utente esegue il comando
