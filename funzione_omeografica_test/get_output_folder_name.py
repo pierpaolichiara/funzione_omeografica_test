cb84faae0989707cfeef20097762d9278242e238
pierpaolichiara <pierpaolichiara@gmail.com>
2023-05-29 16:28:05 +0200

    updated get_output_folder.py: added print of the output path

--- funzione_omeografica_test/get_output_folder_name.py
+++ funzione_omeografica_test/get_output_folder_name.py
@@ -27,4 +27,5 @@ def get_output_folder_name(output_folder_path: str = None) -> str:
         # sottocartella di nome "output" nella cartella da cui l'utente esegue il comando
         current_working_dir = os.getcwd()
         cartella_output = os.path.join(current_working_dir, "output")
+    print(f"I test verranno salvati in {cartella_output}")
     return cartella_output
