d7777d204787f1bac0d3618895a0f8bf53478f65
pierpaolichiara <pierpaolichiara@gmail.com>
2023-06-02 11:39:33 +0200

    Update test_replace_placeholder.py

--- funzione_omeografica_test/test/test_replace_placeholder.py
+++ funzione_omeografica_test/test/test_replace_placeholder.py
@@ -9,7 +9,11 @@ input = [
          ]
 @pytest.mark.parametrize(('text, placeholder, value, expected_result'), input)
 def test_replace_placeholder(text: str, placeholder: str, value: str, expected_result: str):
-#FIXME: DESCRIZIONE
+    """
+    Dati un testo con un placeholder tra due asterischi, nome placeholder e valore da sostituire al placeholder nel testo,
+    questo test verifica che la funzione 'replace_placeholder' sostituisca correttamente il placeholder e che quindi
+    il testo fornito in output dalla funzione dopo la sostituzione sia quello previsto.
+    """
     final_text=mod.replace_placeholder(text_line=text, placeholder_id=placeholder, placeholder_value=value)
     assert final_text==expected_result
 
