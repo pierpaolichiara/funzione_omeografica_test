66c35384f35c5b66c3b98cff1571a7d0fee3742e
pierpaolichiara <pierpaolichiara@gmail.com>
2023-06-02 11:56:54 +0200

    changed test_get_output_folder: split in 3 tests
    - renamed: test_get_output_folder.py -> test_get_output_folder_name.py
    - split the test for the function get_output_folder_name in 3 different cases:
      > no path in input
      > relative path in input
      > absolute path in input

---
+++ funzione_omeografica_test/test/test_get_output_folder_name.py
@@ -0,0 +1,33 @@
+import os
+import funzione_omeografica_test.get_output_folder_name as mod
+#FIXME:
+def test_no_path_get_output_folder_name():
+    """
+    In assenza di input,
+    il test verifica che la funzione 'get_output_folder_name' produca correttamente una cartella di 'output' all'interno
+    della directory della libreria '..\funzione_omeografica_test'.
+    """
+    folder = ''
+    output_folder = mod.get_output_folder_name(folder)
+    assert output_folder == os.path.join(os.getcwd(), "output")
+
+def test_rel_path_get_output_folder_name():
+    """
+    Dato un path relativo,
+    il test verifica che la funzione 'get_output_folder_name' produca correttamente una cartella di output all'interno
+    della directory della libreria '..\funzione_omeografica_test con il nome indicato in input.
+    """
+    folder = 'nomecartella'
+    output_folder = mod.get_output_folder_name(folder)
+    expected_output = os.path.join(os.getcwd(), "nomecartella")
+    assert output_folder == expected_output
+
+def test_abs_path_get_output_folder_name():
+    """
+    Dato un path assoluto,
+    il test verifica che la funzione 'get_output_folder_name' produca correttamente una cartella di output nel path di input.
+    """
+    init_path = os.path.dirname(os.getcwd())
+    input_path = os.path.join(init_path, "output_assoluto")
+    output_folder = mod.get_output_folder_name(input_path)
+    assert output_folder == input_path
