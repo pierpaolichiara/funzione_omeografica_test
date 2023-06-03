79723de7caf9f6bc0348df81ee726909f8e4caed
pierpaolichiara <pierpaolichiara@gmail.com>
2023-06-03 19:24:41 +0200

    Added new input 'id_classe' in main.py to be associated to the random seed generation of the test

--- funzione_omeografica_test/main.py
+++ funzione_omeografica_test/main.py
@@ -22,6 +22,8 @@ def main():
                         help='Estremi del dominio da cui vengono estratti i parametri della funzione omeografica diversi per ogni studente.')
     parser.add_argument('-a', '--elenco_alunni', type=str, action='store', required=True,
                         help='File excel con elenco degli alunni per i quali generare il test')
+    parser.add_argument('-c', '--id_classe', type=str, action='store', required=True,
+                        help='Codice identificativo della classe, cioe\' dell\'elenco alunni per cui si sta generando il test')
     parser.add_argument('-o', '--cartella_output', type=str, action='store', required=False,
                         help='Cartella dove salvare i test generati automaticamente')
     #assegniamo a 'args.***' ogni dato *** in input riconosciuto
@@ -29,13 +31,18 @@ def main():
 
     #assegniamo gli input ricevuti a delle variabili python, dopo averli resi in una forma piu' funzionale al programma
     estremi_dominio_input = args.estremi_dominio
-    #fixme: modifica sotto non recepita
-    print(f"Gli estremi del dominio sono {estremi_dominio_input}.")
     estremi_dominio = parse_function_domain(domain_extremes=estremi_dominio_input)
+    #fixme: modifica sotto non recepita
+    print(f"Gli estremi del dominio sono {estremi_dominio}.")
 
     #estrazione lista di nomi alunni
     file_elenco_alunni = args.elenco_alunni
     nomi_alunni = parse_student_list(student_list_path=file_elenco_alunni)
+
+    #identificativo classe da inserire in random.seed
+    id_classe = args.id_classe
+    #fixme: modifica sotto non recepita
+    print(f"I test verranno associati all'identificativo id_classe= {id_classe}.")
 
     #con la seguente funzione, se viene indicata una cartella in input, si usa quella come cartella_output, altrimenti
     # ne viene creata una
@@ -44,7 +51,7 @@ def main():
     print(f"I test verranno salvati in {cartella_output}.")
 
     #generazione dei test in html a partire dai dati in input
-    generate_tests(template_content_str=TEMPLATE_CONTENT, output_dir=cartella_output, student_lists=nomi_alunni, function_domain=estremi_dominio)
+    generate_tests(template_content_str=TEMPLATE_CONTENT, output_dir=cartella_output, student_lists=nomi_alunni, function_domain=estremi_dominio, class_id=id_classe)
 
 
 if __name__ == "__main__":
